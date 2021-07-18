97b7bdc456968003420105158a6e3d5652107883
ndyabagye <ndyabagyehenrytusi@gmail.com>
2021-07-18 13:12:59 +0300

    initial slack clone build

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBDRK-lH4V3r04-2qYmMnlEwoFgKCVt854",
+    authDomain: "slack-clone-6cd49.firebaseapp.com",
+    projectId: "slack-clone-6cd49",
+    storageBucket: "slack-clone-6cd49.appspot.com",
+    messagingSenderId: "603436825683",
+    appId: "1:603436825683:web:53c0f05997ce2dec88daa6"
+  };
